b18052f4c7085e10aa30259834f31aeea9c8da89
victor131999 <58785449+victor131999@users.noreply.github.com>
2020-07-24 12:43:39 -0500

    Estudiante'

---
+++ Creacion de base de datos Mariadb.sql
@@ -0,0 +1,9 @@
+create database Estudiante;
+
+use Estudiante;
+
+create table estudiante(
+Codigo_est integer(2) primary key, 
+Nombre_est varchar(20), 
+Edad_est integer(2), 
+Direccion_est varchar(20));
